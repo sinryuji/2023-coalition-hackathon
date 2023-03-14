36ee6366b65ea5e3fdf267aa3a4a03e5c3fce1d3
hyeongki <siler2474@gmail.com>
2023-03-14 21:03:34 +0900

    feat: create-post.dtoj

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+//@Module({
+//  imports: [
+//    MongooseModule.forRootAsync({
+//      imports: [ConfigModule],
+//      useFactory: async (configService: ConfigService) => ({
+//        uri: configService.get<string>('MONGO_URL'),
+//      }),
+//      inject: [ConfigService],
+//    }),
+//    PostsModule],
+//  controllers: [AppController],
+//  providers: [AppService],
+//})
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,37 @@
-export class CreatePostDto {}
+import { IsNumber, IsString, IsDate, IsBoolean, IsOptional } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  menu: string;
+
+  @IsOptional()
+  @IsString()
+  content: string;
+
+  @IsNumber()
+  delivery_price: number;
+
+  @IsString()
+  intra_id: string;
+
+  @IsNumber()
+  maximum_people_num: number;
+
+  @IsNumber()
+  current_people_num: number;
+
+  @IsDate()
+  matching_end_date: Date;
+
+  @IsBoolean()
+  avaliable: boolean;
+
+  @IsDate()
+  created_at: Date;
+
+  @IsDate()
+  updated_at: Date;
+}
