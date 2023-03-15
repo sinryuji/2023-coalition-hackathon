afd5df75eb564e78081d6f5037aeb818dcb8b844
Sunwoo Song <sunwoosong@Sunwooui-MacBookAir.local>
2023-03-15 20:29:43 +0900

    login with 42api, save token in localstorage

--- pages/posts/login.tsx
+++ pages/posts/login.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect, FC } from 'react';
-import { useRouter } from 'next/router';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const UID = 'u-s4t2ud-19980a5b8fe8b9e7250f2b1239c325f3f6579c38cc696313f5c761013195ef93';
 const SECRET = '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';
@@ -14,35 +14,46 @@ async function getToken(code: string) {
     code: code,
     redirect_uri: REDIRECT_URI,
   });
+  console.log('in get token: ',data);
   return data.access_token;
 }
 
 function removeCodeFromUrl() {
   const { protocol, host, pathname } = window.location;
+  console.log('removeCode go : ', protocol, host, pathname);
   const newUrl = `${protocol}//${host}${pathname}`;
   window.history.replaceState({}, document.title, newUrl);
 }
 
-const Home: FC = () => {
+export default function Login() {
   const [loggedIn, setLoggedIn] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
+    console.log('useEffect go');
     const { code } = router.query;
+
     if (code) {
-      const tokenString = window.location.toString();
-      const index = tokenString.indexOf('code=');
-      if (index != -1) {        
-        const token = tokenString.substring(index + 5, tokenString.length);
-        console.log('token: ', token);
+      getToken(code as string).then(token => {
         localStorage.setItem('token', token);
-        setLoggedIn(true); 
+        setLoggedIn(true);
         removeCodeFromUrl();
-      }
+      });
     }
   }, [router.query]);
 
-  return <h1>home</h1>;
-};
+  const handleLogin = () => {
+    const authorizationUrl = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    window.location.href = authorizationUrl;
+  };
 
-export default Home;
+  return (
+    <div>
+      <h1>HELLO!</h1>
+      <img src="https://t1.daumcdn.net/cfile/tistory/99C9493C5F32979520" width='250'></img>
+      <br></br>
+      {!loggedIn && <button onClick={handleLogin}>Login with 42</button>}
+      {loggedIn && <p>You are logged in.</p>}
+    </div>
+  );
+}
