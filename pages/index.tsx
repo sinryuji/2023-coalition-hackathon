4eafc4abe27469304ab45210e13ce49695ccdcfb
Sunwoo Song <sunwoosong@Sunwooui-MacBookAir.local>
2023-03-17 11:59:44 +0900

    upload: index page, login page. abstract implement finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, FC } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter, NextRouter } from 'next/router';
+import Cookies from 'js-cookie';
 import axios from 'axios';
 
 const UID = 'u-s4t2ud-19980a5b8fe8b9e7250f2b1239c325f3f6579c38cc696313f5c761013195ef93';
@@ -14,7 +15,7 @@ async function getToken(code: string) {
     code: code,
     redirect_uri: REDIRECT_URI,
   });
-  return data.access_token;
+  return data;
 }
 
 function removeCodeFromUrl() {
@@ -23,24 +24,63 @@ function removeCodeFromUrl() {
   window.history.replaceState({}, document.title, newUrl);
 }
 
+async function setCookieFromCode() {
+  const code = new URLSearchParams(window.location.search).get("code");
+  if (code !== null) {
+    removeCodeFromUrl();
+    try {
+      //const { data } = await getToken(code);
+      const { data } = await axios.post('https://api.intra.42.fr/oauth/token', {
+        grant_type: 'authorization_code',
+        client_id: UID,
+        client_secret: SECRET,
+        code: code,
+        redirect_uri: REDIRECT_URI,
+      });
+      if (data.status >= 400) {
+        throw new Error("Failed to get access token");
+      }
+      Cookies.set("accessToken", data.access_token);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+async function checkIfLoggedIn(router: NextRouter) {
+  const token = Cookies.get('accessToken');
+
+  if (token) {
+    try {
+      const response = await axios.get(
+        "https://api.intra.42.fr/v2/me",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.status >= 400) {
+        throw new Error("Not Authorized");
+      }
+    } catch (error) {
+      router.push("/login");
+    }
+  }
+  else {
+    router.push("/login");
+  }
+}
+
 const Home: FC = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+//  const [loggedIn, setLoggedIn] = useState(false);
   const router = useRouter();
 
+  console.log("home page");
+  setCookieFromCode();
   useEffect(() => {
-    const { code } = router.query;
-    if (code) {
-      const tokenString = window.location.toString();
-      const index = tokenString.indexOf('code=');
-      if (index != -1) {        
-        const token = tokenString.substring(index + 5, tokenString.length);
-        console.log('token: ', token);
-        localStorage.setItem('token', token);
-        setLoggedIn(true); 
-        removeCodeFromUrl();
-      }
-    }
-  }, [router.query]);
+    checkIfLoggedIn(router);
+  }, [router]);
 
   return <h1>home</h1>;
 };
---
+++ pages/login.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const UID = 'u-s4t2ud-19980a5b8fe8b9e7250f2b1239c325f3f6579c38cc696313f5c761013195ef93';
+const SECRET = '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';
+const REDIRECT_URI = "http://localhost:3000";
+
+const Login = () => {
+  const router = useRouter();
+
+  const token = Cookies.get('accessToken');
+  if (token) {
+    window.location.href = "http://localhost:3000";
+  }
+  // const [accessToken, setAccessToken] = useState("");
+
+  const handleLogin = async () => {
+    const url = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    window.location.assign(url);
+  };
+
+  return (
+    <div>
+      <h1>HELLO!</h1>
+      <img src="https://t1.daumcdn.net/cfile/tistory/99C9493C5F32979520" width='250'></img>
+      <br></br>
+      <button onClick={handleLogin}>Login button</button>
+    </div>
+  );
+};
+
+export default Login;
